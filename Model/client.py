3c36228f62471560ddb3c7a7abcccced9b771dc9
uchoagab <86251594+uchoagab@users.noreply.github.com>
2023-12-19 19:28:57 -0300

    fix: Acesso q-table
    Adição de uso de q-table prévia; alteração na consulta da q-table.

--- Model/client.py
+++ Model/client.py
@@ -8,13 +8,30 @@ REWARD_WIN = 300
 num_states = 96
 num_actions = 3  # left, right, jump
 
-q_table = np.zeros((num_states, num_actions))
+starter = input("Utilizar q-table nova?(Y/N) ")
+
+if starter.upper() == "Y":
+    q_table = [[0.0 for x in range(num_actions)] for y in range(num_states)]
+
+elif starter.upper() == "N":
+    q_table = open('q_table.txt', 'r')
+    q_table = q_table.readlines()
+    new_q = []
+
+    for line in q_table:
+        line = line.replace('\n', '')
+        line = line.split(' ')
+        line[0], line[1], line[2] = float(line[0]), float(line[1]), float(line[2])
+        new_q.append(line)
+
+    q_table = new_q
+print(q_table)
 
 
-alpha = 0.1  # taxa de aprendizado
-gamma = 0.9  # fator de desconto
-epsilon = 0.1  # epsilon-greedy
-num_episodes = 500
+alpha = 0.1  #Taxa de aprendizado
+gamma = 0.9  #Fator de desconto
+epsilon = 0.1  #epsilon-greedy
+num_episodes = 10
 
 # Função para extrair informações do estado binário
 def extract_state_info(binary_state):
@@ -24,7 +41,7 @@ def extract_state_info(binary_state):
 
 # Função para mapear plataforma e direção para um estado único
 def map_to_single_state(platform, direction):
-    return (platform * 4) + direction
+    return (platform * 4) + direction 
 
 def check_episode_done(reward):
     if reward == REWARD_FALL or reward == REWARD_WIN: return True
@@ -52,9 +69,9 @@ for episode in range(num_episodes):
         
         # atualização da q-table
         best_next_action = np.argmax(q_table[next_state])
-        q_table[current_state, action] = (1 - alpha) * q_table[current_state, action] + alpha * (reward + gamma * q_table[next_state, best_next_action])
+        q_table[current_state][action] = (1 - alpha) * q_table[current_state][action] + alpha * (reward + gamma * q_table[next_state][best_next_action])
         
-        # salvar q-table como .txt
+        # salva q-table como .txt
         txt = open('q_table.txt', 'w')
         for line in range(num_states):
             txt.write(f"{q_table[line][0]:.5f} {q_table[line][1]:.5f} {q_table[line][2]:.5f}\n")
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Q-Learning
+# **Projeto de Q Learning**
+
+### Descrição do problema:
+
+Neste jogo controlamos o personagem *Amongois*, que terá que passar por diversas plataformas para chegar no seu objetivo final que é o bloco preto. Para isso ele pode fazer 3 movimentos, sendo eles, girar para a esquerda, girar para a direita e pular para a frente.
+
+### Execução:
+
+Para executar o modelo, localizado no arquivo “/Model/client.py”, é necessário que o arquivo “/Environment/Aprendizagem por reforço.exe”. A q-table é atualizada a cada episódio do algoritmo, podendo ser resetada ao iniciar o “client.py”.
+
+### Atalhos de teclado:
+
+* 1: aumenta a velocidade do *Amongois*. 
+* 2: diminui a velocidade do *Amongois*. 
+* 3-7: altera o tamanho da tela.
